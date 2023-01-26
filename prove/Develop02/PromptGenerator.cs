689859590954d3f9e9234f4c667bb2e20f2b2af4
letiabelenda <l_abelenda@hotmail.com>
2023-01-26 18:10:11 -0300

    PromptGeneration done

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -5,7 +5,7 @@ public class PromptGenerator
     {
         var random = new Random();
         var prompts = new List<string>{"If I had one thing I could do over today, what would it be?,", "What was the best part of the day?", "Who was the most interesting person I interacted with today?", "How was the weather today?", "Is there anything I did today that I regret?", "In what ways did I serve someone today?"};
-        int index = random.Next(prompts.Count);
+        int index = random.Next(prompts.Count) - 1;
         Console.WriteLine(prompts[index]);
     }
     
