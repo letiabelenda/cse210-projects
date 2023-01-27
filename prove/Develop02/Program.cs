4fa95337de01471b9143c301c1251c0c5453f52c
letiabelenda <l_abelenda@hotmail.com>
2023-01-27 16:02:49 -0300

    Journal change color menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,17 +5,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Welcome to the Journal Program!");
 
         Journal journal = new Journal();
         int option = 0;
         while (option != 5) {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Please select one of the following choices:");
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("1. Write");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("2. Display");
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("3. Load");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("4. Save");
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("5. Quit");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write("What would you like to do? ");
 
             string option_sring = Console.ReadLine();
