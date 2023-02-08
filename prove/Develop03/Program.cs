2f38e43fe0773cd1988abfbb4b6d5cea700c582f
letiabelenda <l_abelenda@hotmail.com>
2023-02-08 20:00:41 -0300

    08022023

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture("");
+        Word word = new Word();
+        Console.WriteLine(scripture);
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,5 +1,19 @@
 public class Scripture
 {
     private Reference _reference;
-    private List<Word> _text;
+    private List<Word> _text = new List<Word>();
+
+    public Scripture(string text){
+        //Word text = new Word();
+        text = "Me gustan los michis";
+         
+        char [] separator = {' '};
+        string[] strlist = text.Split(separator);
+
+        foreach(string s in strlist)
+        {
+            Console.WriteLine(s);
+        }
+        Console.ReadKey();
+    }
 }
