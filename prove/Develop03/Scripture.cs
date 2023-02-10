fbd8f5c639338f6acfd14d0d7574865a92c6efcc
letiabelenda <l_abelenda@hotmail.com>
2023-02-09 21:40:15 -0300

    09022023

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,9 +2,10 @@ public class Scripture
 {
     private Reference _reference;
     private List<Word> _text = new List<Word>();
+    private int _length;
 
     public Scripture(string book, string chapter, string firstVerse, string secondVerse, string rawText){
-         
+
         char [] separator = {' '};
         string[] strlist = rawText.Split(separator);
 
@@ -14,6 +15,8 @@ public class Scripture
             _text.Add(word);
         }
         
+        _length = _text.Count;
+
         if (secondVerse == "")
         {
             Reference reference = new Reference(book, chapter, firstVerse);  
@@ -24,7 +27,19 @@ public class Scripture
         }            
     }
 
-    public void HideWord(){
+    public void HideWords(){
+        int[] numbers = {};
+        Random random = new Random();
+        i = 0;
         
+        while (i < 3)
+        {
+            int index = random.Next(_length);
+            if (numbers.Contains(index)){
+                _text[index].Hide();
+                numbers.Add(index);
+            }
+        }
+
     }
 }
