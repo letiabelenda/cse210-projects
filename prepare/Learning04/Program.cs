472a68111d6fb57d6dc2f195ac4f3f4ddf28f767
letiabelenda <l_abelenda@hotmail.com>
2023-02-15 16:04:17 -0300

    Program done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -10,5 +10,9 @@ class Program
         MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(assignment2.GetSummary());
         Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Walters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
