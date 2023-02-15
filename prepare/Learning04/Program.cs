e4b430fb5633f1b531777035fe16c67529af382e
letiabelenda <l_abelenda@hotmail.com>
2023-02-15 15:41:11 -0300

    Assignment class done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
     }
 }
