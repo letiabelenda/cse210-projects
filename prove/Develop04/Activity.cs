9bddd39631e73a0e27fd29fd50d3033cdabc63ce
letiabelenda <l_abelenda@hotmail.com>
2023-02-21 13:00:01 -0300

    21022023

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,9 +1,9 @@
 public class Activity {
     private string _activityName;
     private string _description;
-    private float _duration;
+    private int _duration;
 
-    public Activity(string activityName, string description, float duration) {
+    public Activity(string activityName, string description, int duration) {
         _activityName = activityName;
         _description = description;
         _duration = duration;
@@ -15,7 +15,37 @@ public class Activity {
     public string GetDescription(){
         return _description;
     }
-    public float GetDuration(){
+    public int GetDuration(){
         return _duration;
     }
+
+    public void DisplayingStartingMessage(){
+        Console.WriteLine($"Welcome to the {_activityName} Activity");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = Int32.Parse(Console.ReadLine());
+
+        Console.Clear();
+        Console.WriteLine("Get Ready...");
+
+        var startTime = DateTime.UtcNow;
+
+        while(DateTime.UtcNow - startTime < TimeSpan.FromSeconds(5))
+        {
+             Console.Write("\\");
+            Thread.Sleep(500);
+            Console.Write("|");
+            Thread.Sleep(500);
+            Console.Write("/");
+            Thread.Sleep(500);
+            Console.Write("-");
+            Thread.Sleep(500);
+        }
+    }
+
+    //public DisplayingEndingMessage(){
+
+    //}
 }
