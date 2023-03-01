c51bd4539e4f99a4d9a5b3a61b23697a212f0445
letiabelenda <l_abelenda@hotmail.com>
2023-03-01 17:32:37 -0300

    Learning05 done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,11 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Shape> shapes = new  List<Shape>();
+        
         Square square = new Square("Red", 3);
-        string color = square.GetColor();
-        double area = square.GetArea();
+        shapes.Add(square);
 
-        Console.WriteLine($"This square is {color} and its area is {area}");
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        shapes.Add(rectangle);
 
+        Circle circle = new Circle("Green", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+        
     }
 }
