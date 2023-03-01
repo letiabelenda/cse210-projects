a9faff3824ec5e5c457ceffec7dadc6afb1a08ab
letiabelenda <l_abelenda@hotmail.com>
2023-03-01 17:00:43 -0300

    01032023

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 3);
+        string color = square.GetColor();
+        double area = square.GetArea();
+
+        Console.WriteLine($"This square is {color} and its area is {area}");
+
     }
 }
